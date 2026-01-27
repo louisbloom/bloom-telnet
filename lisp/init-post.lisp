49e4e2a7f572248bc5a52d370d0e0f9355d4b7e3
Thomas Christensen <thomasc1971@hotmail.com>
2026-01-28 04:24:39 +0700

    Reformat Lisp files

--- lisp/init-post.lisp
+++ lisp/init-post.lisp
@@ -9,29 +9,29 @@
 ;; Calculate visual display length of string, excluding ANSI escape codes
 (defun visual-length (str)
   (if (not (string? str))
-      0
-      (let ((ansi-pattern "\\033\\[[0-9;]*m"))
-	(length (regex-replace-all ansi-pattern str "")))))
+    0
+    (let ((ansi-pattern "\\033\\[[0-9;]*m"))
+      (length (regex-replace-all ansi-pattern str "")))))
 
 ;; Pad string to specified width with trailing spaces
 (defun pad-string (str width)
   (if (not (string? str))
-      ""
-      (let ((visual-len (visual-length str)))
-	(let ((padding-needed (- width visual-len)))
-          (if (<= padding-needed 0)
-              str
-              (let ((result str))
-		(do ((i 0 (+ i 1))) ((>= i padding-needed) result)
-		  (set! result (concat result " ")))))))))
+    ""
+    (let ((visual-len (visual-length str)))
+      (let ((padding-needed (- width visual-len)))
+        (if (<= padding-needed 0)
+          str
+          (let ((result str))
+            (do ((i 0 (+ i 1))) ((>= i padding-needed) result)
+              (set! result (concat result " ")))))))))
 
 ;; Repeat a string N times
 (defun repeat-string (str count)
   (if (<= count 0)
-      ""
-      (let ((result ""))
-	(do ((i 0 (+ i 1))) ((>= i count) result)
-          (set! result (concat result str))))))
+    ""
+    (let ((result ""))
+      (do ((i 0 (+ i 1))) ((>= i count) result)
+        (set! result (concat result str))))))
 
 ;; ============================================================================
 ;; STARTUP MESSAGE
@@ -51,15 +51,15 @@
 
 ;; Show terminal capability summary if color is supported
 (if (> (termcap 'color-level) 0)
-    (progn (terminal-echo "  Terminal: ") (terminal-echo (termcap 'type))
-	   (terminal-echo " (")
-	   (let ((level (termcap 'color-level)))
-	     (cond
-               ((= level 4) (terminal-echo "truecolor"))
-               ((= level 3) (terminal-echo "256-color"))
-               ((= level 2) (terminal-echo "16-color"))
-               (t (terminal-echo "8-color"))))
-	   (terminal-echo ")\r\n")))
+  (progn (terminal-echo "  Terminal: ") (terminal-echo (termcap 'type))
+    (terminal-echo " (")
+    (let ((level (termcap 'color-level)))
+      (cond
+        ((= level 4) (terminal-echo "truecolor"))
+        ((= level 3) (terminal-echo "256-color"))
+        ((= level 2) (terminal-echo "16-color"))
+        (t (terminal-echo "8-color"))))
+    (terminal-echo ")\r\n")))
 
 (terminal-echo (startup-reset))
 
